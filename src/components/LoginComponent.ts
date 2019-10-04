13c12bc9deb33518faa6fe0ba70563f602104819
Dmitry Makhnev <dmitriy.makhnev@jugru.org>
2019-10-04 14:04:13 +0300

    feat(ErrorHandling): go style with multiple calls.

--- src/components/LoginComponent.ts
+++ src/components/LoginComponent.ts
@@ -1,5 +1,6 @@
 import { AbstractComponent, Dependency } from "@renderilnik/core";
 import { AuthorizationService } from "../srvices/AuthorizationService";
+import { UserService } from '../srvices/UserService';
 
 
 export class LoginComponent extends AbstractComponent {
@@ -7,16 +8,23 @@ export class LoginComponent extends AbstractComponent {
   @Dependency
   private authorizationService: AuthorizationService;
 
+  @Dependency
+  private userService: UserService;
+
   isLoading = false;
   isLoggedIn = false;
 
   async getData(): Promise<void> {
     this.isLoading = true;
-    const [, error] = await this.authorizationService.checkAuthorization();
-    this.isLoggedIn = error === null;
-    if (error !== null) {
-      console.log(`[${error.name}]: ${error.message}`);
+    const [, error1] = await this.authorizationService.checkAuthorization();
+    const [, error2] = await this.userService.getUser();
+    this.isLoggedIn = error1 === null && error2 === null;
+    if (error1 !== null) {
+      console.log(`[${error1.name}]: ${error1.message}`);
+    }if (error2 !== null) {
+      console.log(`[${error2.name}]: ${error2.message}`);
     }
+    this.isLoading = false;
   }
 
   render(): string {
